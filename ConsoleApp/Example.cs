160eb1954e91d8a8fd015fa31f079d55b557e990
Shapiro Igor <shapiroigor96@gmail.com>
2020-01-08 18:45:07 +0400

    second programm

--- ConsoleApp/Example.cs
+++ ConsoleApp/Example.cs
@@ -1,1 +1,16 @@
-﻿
+﻿using System;
+class BigDick
+{
+    static void Main()
+    {
+        Random rnd = new Random();
+        int i;
+        for (i = 0; i < 50; i++)
+        {
+            int a = rnd.Next(1, 20);
+            if ((a % 3 == 0) && (a % 5 == 0)) Console.WriteLine("Число " + a + " is a BigDick");
+            else if (a % 3 == 0) Console.WriteLine("Число " + a + " is a Big");
+            else if (a % 5 == 0) Console.WriteLine("Число " + a + " is a Dick");
+        }
+    }
+}
